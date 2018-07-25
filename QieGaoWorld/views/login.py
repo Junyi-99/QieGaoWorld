cb3e8867479302382286c39be9c47f9620806162
ly <chiaki@difuer.com>
2018-07-26 01:08:03 +0800

    权限修改

--- QieGaoWorld/views/login.py
+++ QieGaoWorld/views/login.py
@@ -8,6 +8,7 @@ from django.template.loader import render_to_string
 from django.views.decorators.csrf import ensure_csrf_cookie
 from QieGaoWorld.models import User
 from QieGaoWorld.views.decorator import check_post
+from QieGaoWorld import settings
 
 
 # ajax (ensure csrf cookie)
@@ -42,11 +43,11 @@ def login_verify(request):
             user=f.readline().strip()
             passwd=f.readline().strip()
 
+
         # user = User.objects.filter(username=username, password=password)
-    except MultipleObjectsReturned:
+    except IOError:
         return HttpResponse(dialog('failed', 'danger', '该账号不存在'))
-    finally:
-        pass
+
     if "playername="+username !=user or "password="+password !=passwd:
         return HttpResponse(dialog('failed', 'danger',"用户名或密码错误"))
 
@@ -64,7 +65,14 @@ def login_verify(request):
     request.session['register_time'] = user[0].register_time
     request.session['avatar'] = user[0].avatar
     request.session['permissions'] = user[0].permissions
+    with open("../ops.json","r") as f:
+        user=f.read()
+        a=(json.loads(user))
+        s="%"
+        for b in a:
+            s+=b['name']+"%"
+        if username in s:
+            request.session['permissions']=settings.OP_PERMISSIONS
     request.session.set_expiry(3600)  # 1小时有效期
     return HttpResponse(dialog('ok', 'success', '登陆成功'))
-
 
