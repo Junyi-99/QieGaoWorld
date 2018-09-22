21a225e624aa9da458ca427097c9d6c45a7cacec
ly <chiaki@difuer.com>
2018-09-23 00:46:22 +0800

    头颅定制添加校验

--- QieGaoWorld/views/skull.py
+++ QieGaoWorld/views/skull.py
@@ -15,18 +15,26 @@ def url(request, s):
 
 def skull_del(request):
     id=request.POST.get('id',None)
-    menu=SkullCustomize.objects.get(id=id)
+    menu=SkullCustomize.objects.get(id=id,user_id=request.session['id'])
     
     menu.delete()
     return HttpResponse(dialog('ok', 'success', '删除成功!'))
 
 def skull_add(request):
-    name=request.POST.get('name',None)
-    number=request.POST.get('number',None)
-    content=request.POST.get("content",None)
+    try:
+        name=request.POST.get('name',None)
+        number=int(request.POST.get('number',None))
+        content=request.POST.get("content",None)
+    except ValueError:
+        return HttpResponse(dialog('failed', 'danger', '参数错误'))
+
     info=re.search(r'Id:"[\d\w-]+',content)
+    if info==None or info.group()==None or len(info.group())<4 :
+        return HttpResponse(dialog('failed', 'danger', '头颅代码格式错误'))
     id=info.group()[4:]
     info=re.search(r'Value:"[\d\w]+',content)
+    if info==None or info.group()==None or len(info.group())<7 :
+        return HttpResponse(dialog('failed', 'danger', '头颅代码格式错误'))
     value=info.group()[7:]
 
     menu=SkullCustomize(name=name,user_id=int(request.session['id']),number=number,status=False,content=id+':'+value)
