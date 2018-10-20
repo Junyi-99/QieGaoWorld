4f0a878c83a706b0296b7d112c6ae60221d1d974
ly <chiaki@difuer.com>
2018-10-20 16:36:48 +0800

    分页js

--- static/assets/js/dashboard/my.js
+++ static/assets/js/dashboard/my.js
@@ -1,3 +1,45 @@
 function abs(number){
     return number < 0 ? -number : number;
 }
+
+function list(url,page=1,type="user",el="#list",dataType="html"){
+    $(el).html("");
+    loading()
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: {page:page,type:type},
+        success: function(data){
+            $(el).html(data);
+            loading()
+        },
+        error:function(){
+            loading(true)
+        },
+        dataType: "html"
+      });
+}
+function loading(error=false){
+    if($("#loading")[0].hidden){
+        $("#loading")[0].hidden=false;
+    }else{
+        $("#loading")[0].hidden=true;
+    }
+    if(error){
+        $("#loading")[0].hidden=true;
+        $("#error")[0].hidden=false;
+    }else{
+        $("#error")[0].hidden=true;
+    }
+}
+$(function(){
+    $("#main_content").on("click",".qg-page",function () {
+        console.log("page")
+        var page=$(this).data("page");
+        var url=$(this).data("url");
+        var type=$(this).data("type");
+        var el=$(this).data("el");
+        var dataType=$(this).data("dataType");
+        list(url,page,type,el,dataType);
+    })
+})
