20699413cac60a6eaf435c5c9958db59a88cb5d1
ly <chiaki@difuer.com>
2018-11-21 20:36:33 +0800

    fix bug

--- QieGaoWorld/views/signin.py
+++ QieGaoWorld/views/signin.py
@@ -115,7 +115,7 @@ def logs_list(request):
     paginator = Paginator(_list, 25)
     _list=paginator.get_page(page)
     for i in range(0,len(_list)):
-        _list[i].localtime=datetime.fromtimestamp(_list[i].time).strftime('%Y-%m-%d %H:%M:%S')
+        _list[i].localtime=datetime.utcfromtimestamp(_list[i].time).strftime('%Y-%m-%d %H:%M:%S')
         _list[i].nickname=username_get_nickname(_list[i].username)
 
     context = {
